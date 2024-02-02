66639b542184aad8b52c492345c86d420f7c3295
khaled shaheen <kshaheen2003@gmail.com>
2024-02-01 23:41:39 -0600

    changed about info

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,44 +1,49 @@
-// import React from "react";
-// import Card from "react-bootstrap/Card";
-// import { ImPointRight } from "react-icons/im";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
 
-// function AboutCard() {
-//   return (
-//     <Card className="quote-card-view">
-//       <Card.Body>
-//         <blockquote className="blockquote mb-0">
-//           <p style={{ textAlign: "justify" }}>
-//             Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-//             from <span className="purple"> Bhubaneswar, India.</span>
-//             <br />
-//             I am currently employed as a software developer at Juspay.
-//             <br />
-//             I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-//             Mesra.
-//             <br />
-//             <br />
-//             Apart from coding, some other activities that I love to do!
-//           </p>
-//           <ul>
-//             <li className="about-activity">
-//               <ImPointRight /> Playing Games
-//             </li>
-//             <li className="about-activity">
-//               <ImPointRight /> Writing Tech Blogs
-//             </li>
-//             <li className="about-activity">
-//               <ImPointRight /> Travelling
-//             </li>
-//           </ul>
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="#4169E1">Khaled Shaheen </span>
+            from <span className="purple"> Dallas, TX</span>
+            <br />
+            <br />
 
-//           <p style={{ color: "rgb(155 126 172)" }}>
-//             "Strive to build things that make a difference!"{" "}
-//           </p>
-//           <footer className="blockquote-footer">Soumyajit</footer>
-//         </blockquote>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
+            I am currently a volunter in a relgious organization as website developer. I am looking forward for oppuroties as a junior Software Engineer!
+            <br />
+            <br />
+            I graduated with honors, earning a Bachelor of Science in Computer Science in December 2023 from St. Cloud State University.
+            <br />
+            <br />
+            I am now eagerly seeking to pursue a Master's degree in Artificial Intelligence to further expand my knowledge and expertise in this dynamic field.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games (Fornite, Fifa, etc.)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Working Out
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading 
+            </li>
+          </ul>
 
-// export default AboutCard;
+          <p style={{ color: "#1E90FF", fontStyle: "italic" }}>
+            "For the things we have to learn before we can do them, we learn by doing them."{" "}
+          </p>
+          <footer className="blockquote-footer">Aristotle</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
