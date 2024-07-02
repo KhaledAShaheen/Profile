0b3d31667219ed044c5e8e184c1808c0d88f52b2
khaled shaheen <kshaheen2003@gmail.com>
2024-07-02 12:45:28 -0500

    sd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Sams Shaheen</strong>
+                <strong className="main-name"> s Shaheen</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
