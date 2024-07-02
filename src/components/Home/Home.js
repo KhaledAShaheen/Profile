e1ba55870302021b59e38bb78f39903bab38566a
khaled shaheen <kshaheen2003@gmail.com>
2024-07-02 12:36:57 -0500

    kl

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There Man!!!{" "}
+                Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Khaled Shaheen</strong>
+                <strong className="main-name"> Sams Shaheen</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
